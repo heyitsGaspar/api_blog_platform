a77563a6b019391f2dca3c2872a1e4362dd38924
heyitsGaspar <israel.chay.32@gmail.com>
2024-06-18 10:24:46 -0600

    Token for register

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+
+
 exports.register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -11,12 +13,52 @@ exports.register = async (req, res) => {
 
         const user = await User.create({ username, email, password: hashedPassword });
 
-        res.status(201).json({ message: 'User registered successfully', user });
+        // Generar un nuevo token para el usuario registrado
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.status(201).json({ message: 'User registered successfully', user, token });
     } catch (error) {
         console.error('Error registering user:', error);  // Log para depuración
-        res.status(500).json({ error: 'Failed to register user' });
+        if (!res.headersSent) {  // Verificar si los encabezados ya han sido enviados
+            res.status(500).json({ error: 'Failed to register user' });
+        }
     }
 };
+
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        // Generar el token JWT
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ error: 'Failed to login' });
+    }
+};
+
+exports.getUser = async (req, res) => {
+    try {
+        const userId = req.params.id; // Obtener el ID del usuario de los parámetros de la solicitud
+        const user = await User.findByPk(userId); // Buscar el usuario por su ID
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ user });
+    } catch (error) {
+        console.error('Error getting user:', error);
+        res.status(500).json({ error: 'Failed to get user' });
+    }
+};
+
 
 exports.login = async (req, res) => {
     try {
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -10,6 +10,7 @@ module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
+    console.log('UserId from JWT:', req.userId);
     next();
   } catch (error) {
     res.status(401).json({ error: 'Invalid token' });
